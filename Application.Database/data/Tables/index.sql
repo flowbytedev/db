5369493118cd7dc9c4009397a3f258eee3d01731
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-21 21:39:35 +0200

    col: remove move storage cols to storage usage tables

--- Application.Database/data/Tables/index.sql
+++ Application.Database/data/Tables/index.sql
@@ -7,8 +7,6 @@
 	[name]					NVARCHAR (200) NOT NULL,
 	[columns]				NVARCHAR (200) NULL,
 	[type]					NVARCHAR (200) NULL,
-	[used_space_mb]			DECIMAL(38,20) NULL,
-	[allocated_space_mb]	DECIMAL(38,20) NULL, 
     CONSTRAINT [PK_index] PRIMARY KEY ([host], [database_name], [schema], [table_name]) ON [FLOWBYTE_DIM], 
     CONSTRAINT [FK_index_table] FOREIGN KEY ([host], [database_name], [schema], [table_name]) REFERENCES [data].[table]([host], [database_name], [schema], [name])
 )  ON [FLOWBYTE_DIM];
--- Application.Database/data/Tables/table.sql
+++ Application.Database/data/Tables/table.sql
@@ -4,9 +4,6 @@
 	[database_name]            NVARCHAR (200) NOT NULL,
 	[schema]              NVARCHAR (200) NOT NULL,
 	[name]                NVARCHAR (200) NOT NULL,
-	[row_count]           INT  NOT NULL,
-	[used_space_mb]       DECIMAL(38,20) NULL,
-	[allocated_space_mb]  DECIMAL(38,20) NULL, 
 	CONSTRAINT [PK_table] PRIMARY KEY ([host], [database_name], [schema], [name]) ON [FLOWBYTE_DIM],
     CONSTRAINT [FK_table_database] FOREIGN KEY ([host], [name]) REFERENCES [data].[database]([host], [name])
 )  ON [FLOWBYTE_DIM];

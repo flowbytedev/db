663dc5865651ae00d4c4d6637ed8e24ef0b2108f
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-22 22:58:22 +0200

    col: update table storage usage columns

--- Application.Database/data/Tables/table_storage_usage.sql
+++ Application.Database/data/Tables/table_storage_usage.sql
@@ -2,10 +2,9 @@
 (
 	[date]				  DATETIME NULL,
 	[host]                NVARCHAR (200) NOT NULL,
-	[database]            NVARCHAR (200) NOT NULL,
-	[object_id]           NVARCHAR (200) NOT NULL,
+	[database_name]            NVARCHAR (200) NOT NULL,
 	[schema]              NVARCHAR (200) NOT NULL,
-	[name]                NVARCHAR (200) NOT NULL,
+	[table_name]                NVARCHAR (200) NOT NULL,
 	[row_count]           INT  NOT NULL,
 	[used_space_mb]       DECIMAL(38,20) NULL,
 	[allocated_space_mb]  DECIMAL(38,20) NULL, 

74d60645eca5a53491ec7c00f61700554cb123c0
Kevork Keheian <kevorkkeheian@gmail.com>
2025-02-25 21:34:47 +0200

    fk: fix table foreign key

--- Application.Database/data/Tables/table.sql
+++ Application.Database/data/Tables/table.sql
@@ -5,5 +5,5 @@
 	[schema]              NVARCHAR (200) NOT NULL,
 	[name]                NVARCHAR (200) NOT NULL,
 	CONSTRAINT [PK_table] PRIMARY KEY ([host], [database_name], [schema], [name]) ON [FLOWBYTE_DIM],
-    CONSTRAINT [FK_table_database] FOREIGN KEY ([host], [name]) REFERENCES [data].[database]([host], [name])
+    CONSTRAINT [FK_table_database] FOREIGN KEY ([host], [database_name]) REFERENCES [data].[database]([host], [name])
 )  ON [FLOWBYTE_DIM];

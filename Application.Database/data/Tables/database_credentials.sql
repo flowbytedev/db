cfe67d2053c7e9ea8e35a5bfb6a45c74c079233a
Kevork Keheian <kevorkkeheian@gmail.com>
2025-03-03 10:00:40 +0200

    fix: fix database credential col and pk

--- Application.Database/data/Tables/database_credentials.sql
+++ Application.Database/data/Tables/database_credentials.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE [data].[database_credentials]
 (
-	[database_id]    NVARCHAR (200) NOT NULL,
+	[host]    NVARCHAR (200) NOT NULL,
+	[database_name] NVARCHAR(200) NOT NULL,
 	[username]       NVARCHAR (200) NOT NULL,
-	[password]       NVARCHAR (200) NOT NULL
+	[password]       NVARCHAR (200) NOT NULL, 
+    CONSTRAINT [PK_database_credentials] PRIMARY KEY ([host], [database_name], [username])  ON [FLOWBYTE_DIM], 
+    CONSTRAINT [FK_database_credentials_host] FOREIGN KEY ([host]) REFERENCES [data].[host]([ip_address]), 
+    CONSTRAINT [FK_database_credentials_database] FOREIGN KEY ([host], [database_name]) REFERENCES [data].[database]([host], [name]), 
 )  ON [FLOWBYTE_DIM];

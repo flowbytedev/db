1dfd42997067dde66ccfd96466b1333b1007b246
PeterElHage <127479038+PeterElHage@users.noreply.github.com>
2025-03-04 11:21:09 +0200

    schema: update tables schema

--- Application.Database/data/Tables/table.sql
+++ Application.Database/data/Tables/table.sql
@@ -4,6 +4,7 @@
 	[database_name]            NVARCHAR (200) NOT NULL,
 	[schema]              NVARCHAR (200) NOT NULL,
 	[name]                NVARCHAR (200) NOT NULL,
+	[row_count]           DECIMAL(38,20) NULL,
 	CONSTRAINT [PK_table] PRIMARY KEY ([host], [database_name], [schema], [name]) ON [FLOWBYTE_DIM],
     CONSTRAINT [FK_table_database] FOREIGN KEY ([host], [database_name]) REFERENCES [data].[database]([host], [name])
 )  ON [FLOWBYTE_DIM];
--- Application.Database/data/Tables/table_storage_usage.sql
+++ Application.Database/data/Tables/table_storage_usage.sql
@@ -1,11 +1,11 @@
-﻿CREATE TABLE [dbo].[table_storage_usage]
+﻿CREATE TABLE [data].[table_storage_usage]
 (
 	[date]				  DATETIME NULL,
 	[host]                NVARCHAR (200) NOT NULL,
-	[database_name]            NVARCHAR (200) NOT NULL,
+	[database_name]       NVARCHAR (200) NOT NULL,
 	[schema]              NVARCHAR (200) NOT NULL,
-	[table_name]                NVARCHAR (200) NOT NULL,
-	[row_count]           INT  NOT NULL,
+	[table_name]          NVARCHAR (200) NOT NULL,
+	[row_count]           INT NULL,
 	[used_space_mb]       DECIMAL(38,20) NULL,
 	[allocated_space_mb]  DECIMAL(38,20) NULL, 
     

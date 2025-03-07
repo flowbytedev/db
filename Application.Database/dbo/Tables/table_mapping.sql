5e2b4d1ba376c94f2fcddc86800d2600b13c4f14
Kevork Keheian <kevorkkeheian@gmail.com>
2025-03-08 00:44:56 +0200

    col: add source and destination schema to mapping tables

--- Application.Database/dbo/Tables/table_mapping.sql
+++ Application.Database/dbo/Tables/table_mapping.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[table_mapping] (
     [source_host]              NVARCHAR (255) NULL,
     [source_database]          NVARCHAR (255) NULL,
+    [source_schema]             NVARCHAR(20) NULL, 
     [source_table]             NVARCHAR (255) NULL,
     [source_api_endpoint]      NVARCHAR (MAX) NULL,
     [destination_host]         NVARCHAR (255) NULL,
     [destination_database]     NVARCHAR (255) NULL,
+    [destination_schema]        NVARCHAR(20) NULL,
     [destination_table]        NVARCHAR (255) NULL,
     [destination_api_endpoint] NVARCHAR (MAX) NULL,
     [query]                    NVARCHAR (MAX) NULL,
@@ -13,7 +15,9 @@
     [temp_table_name]          NVARCHAR (255) NULL,
     [is_incremental]           TINYINT        DEFAULT ((0)) NOT NULL,
     [incremental_column]       NVARCHAR (255) NULL, 
-    [sequence] INT NULL, 
-    [is_deleted] TINYINT NOT NULL DEFAULT ((0))
+    [sequence]                  INT NULL, 
+    [is_deleted]                TINYINT NOT NULL DEFAULT ((0)), 
+    
+    
 ) ON [FLOWBYTE_DIM] TEXTIMAGE_ON [FLOWBYTE_DIM];
 

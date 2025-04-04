1e1643ea6f7d7be21ae826a001961dbe864bda59
Kevork Keheian <kevorkkeheian@gmail.com>
2025-04-04 13:42:38 +0300

    table: add query_detail table

---
+++ Application.Database/data/Tables/query_detail.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [data].[query_detail]
+(
+	[Id] INT NOT NULL IDENTITY(1,1), 
+    [query] NVARCHAR(MAX) NULL, 
+    [description] NVARCHAR(255) NULL, 
+    [additional_condition] NVARCHAR(MAX) NULL 
+)

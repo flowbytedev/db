1bbd156145c08d6a47f3e448537f2ae6e639f115
Kevork Keheian <kevorkkeheian@gmail.com>
2025-04-04 13:42:21 +0300

    table: add job_trigger_table

---
+++ Application.Database/data/Tables/data_job_trigger.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [data].[data_job_trigger]
+(
+	[Id] INT NOT NULL IDENTITY(1,1), 
+    [job_id] INT NULL, 
+    [trigger_type] NVARCHAR(100) NULL, 
+    [triggered_by] NVARCHAR(255) NULL
+)

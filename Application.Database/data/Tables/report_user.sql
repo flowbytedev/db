9d9d729ff6e760d427a379c2f48bf17240048e75
Kevork Keheian <kevorkkeheian@gmail.com>
2025-05-11 16:02:22 +0300

    fk: fikx report user - report foreign key

--- Application.Database/data/Tables/report_user.sql
+++ Application.Database/data/Tables/report_user.sql
@@ -9,6 +9,6 @@
 	[role]             NVARCHAR(500) NOT NULL,
 
     CONSTRAINT [PK_report_user] PRIMARY KEY ([workspace_id], [application], [report_id], [e-mail]) ON [FLOWBYTE_DIM],
-	CONSTRAINT [FK_report_user_report] FOREIGN KEY ([workspace_id], [application], [report_id]) REFERENCES [data].[report]([workspace_id], [application], [id])
+	CONSTRAINT [FK_report_user_report] FOREIGN KEY ([report_id], [workspace_id], [application]) REFERENCES [data].[report]([id], [workspace_id], [application])
 
 ) ON [FLOWBYTE_DIM];
